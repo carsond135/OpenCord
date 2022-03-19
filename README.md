bad436d6df7dcbf0a5aa1fdf7984fc98e67b8af0
Carson B Dunlap <cbd135.dev@gmail.com>
2022-03-19 16:14:43 -0400

    first round of files for upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# OpenCord
+[![Discord](https://img.shields.io/discord/885879572447522817.svg?color=blue&label=OpenCord&logo=discord&style=for-the-badge)](https://discord.gg/3y6vbneMsW)
+
+An open-source reimplementation of the Discord Android app.
+
+Chat                            |  Drawer
+:------------------------------:|:------------------------------:
+ ![](media/screenshot_chat.png) | ![](media/screenshot_drawer.png)
+
+
+Why does this exist?
+-------
+Current Discord app sucks as it doesn't support any type of customization (themes, plugins, layout etc...). The goal of this
+project is to reimagine the Discord experience on mobile devices.
+
+Status of OpenCord
+-------
+- [ ] Full (or at least most essential) Discord feature implementation
+- [ ] Theme support
+- [ ] Plugin support
+- [ ] Multiplatform support (Windows, macOS, Linux, Android)
+
+FAQ
+-------
+### Will this client support themes/plugins?
+Yes, but my first priority is to implement most of the stock client's features
+
+### Is this against Discord TOS?
+Unfortunately, it is. Although I'm pretty sure Discord won't start banning anyone
+for using OpenCord, considering how many mods of the stock clients already exist.
+Even with that, use this client at your own risk.
+
+### Where download???!?!?!?!?
+Head over to [repository actions](https://github.com/X1nto/OpenCord/actions), click the latest run and download the `app` artifact.
+After downloading, unzip it. Inside you'll find 2 files: `app-discord-debug.apk` and `app-fosscord-debug.apk`.
+
+#### Differences between these versions
+The `discord` version is considered as the standard version of the app, while the `fosscord` version is meant to be used by users familiar with [Fosscord](https://github.com/fosscord/fosscord).
+Fosscord version of the app redirects to Fosscord API, CDN and Gateway instead of Discord's URLs, allowing you to fully experience OpenCord without being scared of getting banned by Discord.
+
+### Stable version wen?
+Soon™
---
+++ app/build.gradle.kts
@@ -0,0 +1,136 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+val composeVersion = "1.1.0~rc01"
+
+android {
+    compileSdk = 31
+
+    flavorDimensions.add("api")
+
+    defaultConfig {
+        applicationId = "com.xinto.opencord"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        buildConfigField("int", "DISCORD_VERSION_CODE", "89108")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    productFlavors {
+        create("discord") {
+            dimension = "api"
+
+            isDefault = true
+
+            buildConfigField("String", "URL_API", "\"https://discord.com/api/v9\"")
+            buildConfigField("String", "URL_CDN", "\"https://cdn.discordapp.com\"")
+            buildConfigField("String", "URL_GATEWAY", "\"wss://gateway.discord.gg/?v=9&encoding=json\"")
+        }
+
+        create("fosscord") {
+            dimension = "api"
+
+            applicationIdSuffix = ".fosscord"
+            versionNameSuffix = "-fosscord"
+
+            buildConfigField("String", "URL_API", "\"https://api.fosscord.com/v9\"")
+            buildConfigField("String", "URL_CDN", "\"https://cdn.fosscord.com\"")
+            buildConfigField("String", "URL_GATEWAY", "\"wss://gateway.fosscord.com/?v=9&encoding=json\"")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = freeCompilerArgs +
+                "-Xopt-in=kotlin.RequiresOptIn"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-rc02"
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.preference:preference-ktx:1.1.1")
+
+    implementation("androidx.compose.compiler:compiler:1.1.0-rc02")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui-util:$composeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    val accompanistVersion = "0.22.0-rc"
+    implementation("com.google.accompanist:accompanist-navigation-animation:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-insets:$accompanistVersion")
+
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    implementation("com.github.hcaptcha:hcaptcha-android-sdk:1.1.0")
+
+    val exoplayerVersion = "2.16.0"
+    implementation("com.google.android.exoplayer:exoplayer-core:$exoplayerVersion")
+    implementation("com.google.android.exoplayer:exoplayer-dash:$exoplayerVersion")
+    implementation("com.google.android.exoplayer:exoplayer-ui:$exoplayerVersion")
+
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    val okHttpVersion = "4.9.1"
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
+
+    implementation("com.github.X1nto:OverlappingPanelsCompose:1.2.0")
+
+    implementation("io.insert-koin:koin-androidx-compose:3.1.3")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+}
